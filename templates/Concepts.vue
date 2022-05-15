305e9359271f1ccdf23460ff018032fe94a0421b
Jabelic <hibiki990523@gmail.com>
2022-05-16 02:05:49 +0900

    ADD: concepts in locale

--- templates/Concepts.vue
+++ templates/Concepts.vue
@@ -2,17 +2,22 @@
   <div>
     <section id="Shops" class="grey lighten-3">
       <v-container>
-        <h2 class="display-1 font-weight-bold mb-3">Artist</h2>
+        <h2 class="display-1 font-weight-bold mb-3">Concepts</h2>
         <Divider />
+        <div class="d-flex flex-column justify-center">
+          <div>{{ concepts }}</div>
+        </div>
       </v-container>
     </section>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
-
+import { concepts } from '@/locale/description'
 export default defineComponent({
-  setup() {},
+  setup() {
+    return { concepts }
+  },
 })
 </script>
 <style scoped></style>
